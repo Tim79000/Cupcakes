1205ba05e99d0b7ce858d134612a55eb1c1adf7a
Tim7900 <100736284+Tim79000@users.noreply.github.com>
2022-03-23 16:19:47 -0800

    Add new cupcake colors!
    TASTY!!!

--- init.lua
+++ init.lua
@@ -83,6 +83,175 @@ minetest.register_craft({
     }
 })
 
+--Cyan
+
+minetest.register_craftitem("cupcakes:cyan_cupcake", {
+    description = "Cyan Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:cyan:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:cyan_cupcake",
+    recipe = {
+        {"dye:cyan"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Grey
+
+minetest.register_craftitem("cupcakes:grey_cupcake", {
+    description = "Grey Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:grey:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:grey_cupcake",
+    recipe = {
+        {"dye:grey"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Black
+
+minetest.register_craftitem("cupcakes:black_cupcake", {
+    description = "Black Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:black:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:black_cupcake",
+    recipe = {
+        {"dye:black"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Brown
+
+minetest.register_craftitem("cupcakes:brown_cupcake", {
+    description = "Brown Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:orange:100^[colorize:black:100^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:brown_cupcake",
+    recipe = {
+        {"dye:brown"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Green
+
+minetest.register_craftitem("cupcakes:green_cupcake", {
+    description = "Green Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:green:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:green_cupcake",
+    recipe = {
+        {"dye:green"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Orange
+
+minetest.register_craftitem("cupcakes:orange_cupcake", {
+    description = "Orange Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:orange:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:orange_cupcake",
+    recipe = {
+        {"dye:orange"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Yellow
+
+minetest.register_craftitem("cupcakes:yellow_cupcake", {
+    description = "Yellow Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:yellow:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:yellow_cupcake",
+    recipe = {
+        {"dye:yellow"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Magenta
+
+minetest.register_craftitem("cupcakes:magenta_cupcake", {
+    description = "Magenta Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:magenta:200^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:magenta_cupcake",
+    recipe = {
+        {"dye:magenta"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Dark grey
+
+minetest.register_craftitem("cupcakes:dark_grey_cupcake", {
+    description = "Dark Grey Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:black:100^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:dark_grey_cupcake",
+    recipe = {
+        {"dye:dark_grey"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
+
+--Dark green
+
+minetest.register_craftitem("cupcakes:dark_green_cupcake", {
+    description = "Dark Green Cupcake",
+    inventory_image = "cupcake_texture.png^[colorize:green:100^[colorize:black:100^cupcake_paper_texture.png",
+    on_use = minetest.item_eat(8),
+})
+
+minetest.register_craft({
+    output = "cupcakes:dark_green_cupcake",
+    recipe = {
+        {"dye:dark_green"},
+        {"farming:bread"},
+        {"default:paper"}
+    }
+})
 
 
 
